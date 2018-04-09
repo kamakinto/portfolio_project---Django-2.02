58fd7718b8edf0a039177756304072be56c01145
Robbie Robinson <haki.projects@gmail.com>
2018-04-09 16:36:01 +0200

    created blogs and other sections
    all in the title

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Blog(models.Model):
+  image = models.ImageField(upload_to='images/')
+  pub_date = models.DateField(auto_now_add=True)
+  last_modified = models.DateField(auto_now=True)
+  author=models.CharField(max_length=100)
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=400)
+  body= models.TextField()
+
 # Create your models here.
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'hitokiru',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
